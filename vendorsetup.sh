f98ced5f1d97e7dee7dadc35e9c0a9f4bcd3a5db
justkelvin <76160352+justkelvin@users.noreply.github.com>
2025-01-28 17:25:41 +0000

    spes: vendorsetup: Add MIUI Camera source
    - Add MIUI Camera source from ItzDFPlayer's GitLab
    - Set branch to leica-5.0
    - Fix title alignment in ASCII art header
    
    Signed-off-by: justkelvin
    Date: 2025-01-28 17:18:18

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -104,15 +104,16 @@ main() {
     clear
     printf "${BOLD}${MAGENTA}
  ╔═══════════════════════════════════════╗
- ║   Android ROM Source Preparation     ║
+ ║   Android ROM Source Preparation      ║
  ╚═══════════════════════════════════════╝
 ${RESET}\n"
 
     # Configuration Variables
     VENDOR_BRANCH="15.0"
     KERNEL_BRANCH="vauxite"
-    HARDWARE_BRANCH="lineage-22.0"
+    HARDWARE_BRANCH="15"
     DEBUG_BRANCH="lineage-22"
+    MIUI_CAMERA_BRANCH="leica-5.0"
 
     # Cleanup Steps
     log_start "Preparing device sources..."
@@ -123,20 +124,24 @@ ${RESET}\n"
 
     # Cloning Sources
     check_and_clone "vendor/xiaomi/spes" \
-        "https://github.com/spes-development/vendor_xiaomi_spes" \
+        "git@github.com:justkelvin/vendor_xiaomi_spes.git" \
         "$VENDOR_BRANCH" "Vendor"
 
     check_and_clone "kernel/xiaomi/sm6225" \
-        "https://github.com/CHRISL7/kernel_xiaomi_sm6225.git" \
+        "git@github.com:justkelvin/kernel_xiaomi_sm6225.git" \
         "$KERNEL_BRANCH" "Kernel"
 
     check_and_clone "hardware/xiaomi" \
-        "https://github.com/LineageOS/android_hardware_xiaomi" \
+        "https://github.com/ProjectPixelage/android_hardware_xiaomi.git" \
         "$HARDWARE_BRANCH" "Hardware"
 
     check_and_clone "hardware/samsung-ext/interfaces" \
         "https://github.com/spes-development/hardware_samsung-extra_interfaces" \
         "$DEBUG_BRANCH" "Debugging Tools"
+    
+    check_and_clone "vendor/xiaomi/miuicamera" \
+        "https://gitlab.com/ItzDFPlayer/vendor_xiaomi_miuicamera.git" \
+        "$MIUI_CAMERA_BRANCH" "MIUI Camera"
 
     # Final Status
     printf "\n${BOLD}${GREEN}
